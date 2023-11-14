2d45d842fa2b56dae38eab54b340965f693fe26b
rogercriollo <rlcriollo@utpl.edu.ec>
2023-11-14 09:42:29 -0500

    examen1

---
+++ app/src/main/java/com/example/examen/MainActivity.kt
@@ -0,0 +1,43 @@
+
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.example.examen.R
+
+class MainActivity : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        llamarReg()
+
+    }
+    fun llamarReg(){
+
+        val btnCamara = findViewById<Button>(R.id.btnCamara)
+        btnCamara.setOnClickListener {
+            startForResult.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+        }
+    }
+
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent = result.data
+                val imageBitmap = intent?.extras?.get("data") as Bitmap
+                val imageView = findViewById<ImageView>(R.id.imageView)
+                imageView.setImageBitmap(imageBitmap)
+            }
+        }
+}
